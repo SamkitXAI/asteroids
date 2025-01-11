9dd7c4306dda86d94b6c2909578470fb79b0ae8b
SamkitXAI <sammodtab@gmail.com>
2025-01-11 13:05:37 +0000

    Made the player game part

--- main.py
+++ main.py
@@ -2,15 +2,42 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import *
 from constants import *
 
 def main():
     pygame.init()
     print("Starting asteroids!")
+    
+    clock_fps = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()  # Group A
+    drawable = pygame.sprite.Group()  # Group B
+    
+    Player.containers = (updatable, drawable)
+
+    # Set up the screen with defined width and height
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Create a Player object at the center of the screen
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
+    
+
     while True:
         screen.fill((0, 0, 0))
+        for entity in drawable:
+            entity.draw(screen)
+        for entity in updatable:
+            entity.update(dt)
+            
         pygame.display.flip()
+        
+        
+        dt = clock_fps.tick(60)/1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
